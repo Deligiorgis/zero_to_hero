8f5254cc0efa78ce0d07f844faad479efe372347
Georgios Deligiorgis <delig0805@gmail.com>
2021-08-31 18:55:38 +0200

    improve blobs and move outside common code with fashionMNIST

--- src/zero_to_hero/data/blobs/datamodule.py
+++ src/zero_to_hero/data/blobs/datamodule.py
@@ -10,6 +10,7 @@ from sklearn.datasets import make_blobs
 from torch.utils.data import DataLoader, Dataset
 
 from zero_to_hero.data.blobs.dataset import BlobsDataset
+from zero_to_hero.data.statistics import standardize_step
 
 
 class BlobsDataModule(pl.LightningDataModule):
@@ -22,15 +23,21 @@ class BlobsDataModule(pl.LightningDataModule):
 
         self.config = config
 
-        train_n_samples = int(self.config["n_samples"] * self.config["train_ratio"])
-        valid_n_samples = int(self.config["n_samples"] * self.config["valid_ratio"])
-        test_n_samples = self.config["n_samples"] - train_n_samples - valid_n_samples
+        train_n_samples = int(self.config["data"]["n_samples"] * self.config["data"]["train_ratio"])
+        valid_n_samples = int(self.config["data"]["n_samples"] * self.config["data"]["valid_ratio"])
+        test_n_samples = self.config["data"]["n_samples"] - train_n_samples - valid_n_samples
 
-        n_centers = len(self.config["centers"])
+        n_centers = len(self.config["data"]["centers"])
 
         train_data, train_targets = self.get_data_and_targets(int(train_n_samples / n_centers))
         valid_data, valid_targets = self.get_data_and_targets(int(valid_n_samples / n_centers))
         test_data, test_targets = self.get_data_and_targets(int(test_n_samples / n_centers))
+
+        train_data, valid_data, test_data = standardize_step(  # type: ignore # False positive mypy
+            train_data=train_data,
+            valid_data=valid_data,
+            test_data=test_data,
+        )
 
         self.train_dataset = BlobsDataset(
             data=train_data,
@@ -52,12 +59,18 @@ class BlobsDataModule(pl.LightningDataModule):
         :return: (features, target)
         """
         data, targets = [], []
-        for enum, center in enumerate(self.config["centers"]):
+        for enum, center in enumerate(self.config["data"]["centers"]):
+            print(
+                f"Label: {enum} "
+                f"Center: {center} "
+                f"Number of samples: {n_samples} "
+                f"Number of features: {self.config['data']['n_features']}"
+            )
             features, target, *_ = make_blobs(
                 n_samples=n_samples,
-                n_features=self.config["n_features"],
+                n_features=self.config["data"]["n_features"],
                 centers=[center],
-                cluster_std=self.config["cluster_std"],
+                cluster_std=self.config["data"]["cluster_std"],
                 shuffle=False,
                 random_state=1,
             )
@@ -70,10 +83,10 @@ class BlobsDataModule(pl.LightningDataModule):
         assert isinstance(self.train_dataset, Dataset)
         return DataLoader(
             dataset=self.train_dataset,
-            batch_size=self.config["batch_size"],
+            batch_size=self.config["hyper_parameters"]["batch_size"],
             shuffle=True,
             pin_memory=True,
-            num_workers=self.config["num_workers"],
+            num_workers=self.config["hyper_parameters"]["num_workers"],
             drop_last=False,
         )
 
@@ -81,10 +94,10 @@ class BlobsDataModule(pl.LightningDataModule):
         assert isinstance(self.valid_dataset, Dataset)
         return DataLoader(
             dataset=self.valid_dataset,
-            batch_size=self.config["batch_size"],
+            batch_size=self.config["hyper_parameters"]["batch_size"],
             shuffle=False,
             pin_memory=True,
-            num_workers=self.config["num_workers"],
+            num_workers=self.config["hyper_parameters"]["num_workers"],
             drop_last=False,
         )
 
@@ -92,9 +105,9 @@ class BlobsDataModule(pl.LightningDataModule):
         assert isinstance(self.test_dataset, Dataset)
         return DataLoader(
             dataset=self.test_dataset,
-            batch_size=self.config["batch_size"],
+            batch_size=self.config["hyper_parameters"]["batch_size"],
             shuffle=False,
             pin_memory=True,
-            num_workers=self.config["num_workers"],
+            num_workers=self.config["hyper_parameters"]["num_workers"],
             drop_last=False,
         )
